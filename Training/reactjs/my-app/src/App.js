1858b63424d755ec1bd7aea9960f1c6f09c2b755
seebaah <103080115+seebaah@users.noreply.github.com>
2022-05-06 05:53:38 +0530

    training
    initial commit

---
+++ Training/reactjs/my-app/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+
+<div class="innerdiv">
+  <div class="innerdivh2">
+    <h2>Login</h2>
+    <div class="formelement">
+      <div class="formelement input"><label>Username</label>
+        <input type="text" placeholder="Username"/>
+      </div>
+      <div class="formelement input"><label>Password</label>
+        <input type="password" placeholder="Password"/>
+      </div>
+    </div>
+    <div ><button class="button">LOGIN</button></div>
+
+
+
+  </div>
+</div>
+</div>
+
+
+    </div>
+  );
+}
+
+export default App;
