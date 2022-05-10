0a97c12cf91c9e6527abd479c8b39e32fd5ff973
seebaah <103080115+seebaah@users.noreply.github.com>
2022-05-10 15:31:22 +0530

    Update LoginPage.js
    update

--- Projects/ECommerce/webserver/src/LoginPage.js
+++ Projects/ECommerce/webserver/src/LoginPage.js
@@ -1,24 +1,16 @@
 import "./style.css";
 
+
 function LoginPage() {
   return (
     <div>
-      <div class="container">
-        <div class="innerdiv">
-          <h2>Login</h2>
-          <form class="formelement">
-            <input type="text" placeholder="username" />
-            <br />
-            <br />
-            <input type="password" placeholder="password" />
-            <br />
-            <br />
-            <button>login</button>
-            <br />
-            <br />
-          </form>
-        </div>
-      </div>
+      <h1>LOGIN</h1>
+      <div><label>Username</label><input type="text"/></div>
+      <div><label>Password</label><input type="password"/></div><br/>
+    
+    
+      <button>LOGIN</button>
+      <p className="link">New User?</p>
     </div>
   );
 }
