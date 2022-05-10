5ebbf682e19214a26d19cd8f3cfc229731d22845
seebaah <103080115+seebaah@users.noreply.github.com>
2022-05-10 15:44:46 +0530

    Update LoginPage.js
    commit

--- Projects/ECommerce/webserver/src/LoginPage.js
+++ Projects/ECommerce/webserver/src/LoginPage.js
@@ -1,16 +1,34 @@
 import "./style.css";
-
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
+  const navigate = useNavigate();
+  function newclick(e) {
+    e.preventDefault();
+    navigate("/signup");
+  }
   return (
     <div>
       <h1>LOGIN</h1>
-      <div><label>Username</label><input type="text"/></div>
-      <div><label>Password</label><input type="password"/></div><br/>
-    
-    
+      <div>
+        <label>Username</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" />
+      </div>
+      <br />
+
       <button>LOGIN</button>
-      <p className="link">New User?</p>
+      <p
+        onClick={(e) => {
+          newclick(e);
+        }}
+        className="link"
+      >
+        New User?
+      </p>
     </div>
   );
 }
