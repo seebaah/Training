58dec3a43e22cca90ae9d48056d30891025d241a
seebaah <103080115+seebaah@users.noreply.github.com>
2022-05-11 21:53:40 +0530

    assignment
    commit

---
+++ Assignments/Json1/index1.js
@@ -0,0 +1,24 @@
+var Student=
+[
+    {
+      "id": 101,
+      "name": "John",
+      "dob": 1994,
+      "mark": 300
+    },
+    {
+      "id": 102,
+      "name": "Alex",
+      "dob": 1998,
+      "mark": 270
+    },
+    {
+      "id": 103,
+      "name": "Manu",
+      "dob": 1995,
+      "mark": 290
+    }
+  ]
+  console.log(Student[2].id);
+  console.log(Student[0].mark);
+  console.log(Student[0]);
